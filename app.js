ff6f3dd634bfc1082ebaa4df868a139e653e01b8
Allan Shrestha <stevevai111@hotmail.com>
2022-02-02 15:13:55 +0000

    More changes added

---
+++ app.js
@@ -0,0 +1,39 @@
+const key = document.querySelectorAll(".key-box");
+const box = document.querySelectorAll(".box");
+const enter = document.getElementById("enter");
+
+let newArr = [];
+const correctWord = "RIGHT";
+
+key.forEach(item => {
+  item.addEventListener("click", event => {
+    for (let i = 0; i < 5; i++) {
+      if (box[i].innerHTML === "") {
+        box[i].innerHTML = event.target.innerHTML;
+        newArr.push(event.target.innerHTML);
+        return;
+      }
+    }
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// When any letter key from the keyboard is pressed then it should be displayed on the game box starting from the left and moving horizontally up to five numbers.
+
+// After enter is pressed it should move to the next row if all the letters don't match the correct word.
+
+// After the sixth try if all the letters don't match then the correct word would be displayed
+
+// After any row matches all the words "You won" should be displayed
