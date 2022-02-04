02c6e995c237bd274c8c2ad24b0b8e567b28e68e
Allan Shrestha <stevevai111@hotmail.com>
2022-02-04 11:10:51 +0000

    Logic working through all the boxes

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const enter = document.getElementById("enter");
 let newArr = [];
 const correctWord = "RIGHT";
 
+let counter = 0;
 
-key.forEach(item => {
-  item.addEventListener("click", event => {
+key.forEach((item) => {
+  item.addEventListener("click", (event) => {
     for (let i = 0; i < 5; i++) {
       if (box[i].innerHTML === "") {
         box[i].innerHTML = event.target.innerHTML;
@@ -15,45 +16,47 @@ key.forEach(item => {
         return;
       }
     }
+  });
+});
+
+const guessWord = (firstNum, lastNum) => {
+  newArr.length = 0;
+  key.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      for (let i = firstNum; i < lastNum; i++) {
+        if (box[i].innerHTML === "") {
+          box[i].innerHTML = event.target.innerHTML;
+          newArr.push(event.target.innerHTML);
+          return;
+        }
+      }
+    })
   })
-})
+}
+enter.addEventListener("click", (event) => {
 
-enter.addEventListener("click", event => {
-    let attemptedWord = newArr.join("");
+
+  let attemptedWord = newArr.join("")
+
+  if (attemptedWord === correctWord) {
+    alert("You have won!!!");
+  } else { counter++
+      if (counter===1) {
+        guessWord(5,10);
+      } else if (counter===2) {
+        guessWord(10,15);
+      } else if (counter===3) {
+        guessWord(15,20)
+      } else if (counter===4){
+        guessWord(20,25)
+      } else if (counter===5){
+        guessWord(25,30)
+      } else {
+        alert("The game has ended");
+      }
+    };
+  })
   
-    if ( attemptedWord === correctWord) {
-      alert("You have won!!!");
-    } else {
-      let newArr = [];
-      key.forEach(item => {
-        item.addEventListener("click", event => {
-          for (let i = 5; i < 10; i++) {
-            if (box[i].innerHTML === "") {
-              box[i].innerHTML = event.target.innerHTML;
-              newArr.push(event.target.innerHTML);
-              return;
-            }
-          }
-        })
-      })
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // When any letter key from the keyboard is pressed then it should be displayed on the game box starting from the left and moving horizontally up to five numbers.
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -5,6 +5,7 @@ var box = document.querySelectorAll(".box");
 var enter = document.getElementById("enter");
 var newArr = [];
 var correctWord = "RIGHT";
+var counter = 0;
 key.forEach(function (item) {
   item.addEventListener("click", function (event) {
     for (var i = 0; i < 5; i++) {
@@ -16,27 +17,46 @@ key.forEach(function (item) {
     }
   });
 });
+
+var guessWord = function guessWord(firstNum, lastNum) {
+  newArr.length = 0;
+  key.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      for (var i = firstNum; i < lastNum; i++) {
+        if (box[i].innerHTML === "") {
+          box[i].innerHTML = event.target.innerHTML;
+          newArr.push(event.target.innerHTML);
+          return;
+        }
+      }
+    });
+  });
+};
+
 enter.addEventListener("click", function (event) {
   var attemptedWord = newArr.join("");
 
   if (attemptedWord === correctWord) {
     alert("You have won!!!");
   } else {
-    var _newArr = [];
-    key.forEach(function (item) {
-      item.addEventListener("click", function (event) {
-        for (var i = 5; i < 10; i++) {
-          if (box[i].innerHTML === "") {
-            box[i].innerHTML = event.target.innerHTML;
+    counter++;
 
-            _newArr.push(event.target.innerHTML);
-
-            return;
-          }
-        }
-      });
-    });
+    if (counter === 1) {
+      guessWord(5, 10);
+    } else if (counter === 2) {
+      guessWord(10, 15);
+    } else if (counter === 3) {
+      guessWord(15, 20);
+    } else if (counter === 4) {
+      guessWord(20, 25);
+    } else if (counter === 5) {
+      guessWord(25, 30);
+    } else {
+      alert("The game has ended");
+    }
   }
+
+  ;
 }); // When any letter key from the keyboard is pressed then it should be displayed on the game box starting from the left and moving horizontally up to five numbers.
 // After enter is pressed it should move to the next row if all the letters don't match the correct word.
 // After the sixth try if all the letters don't match then the correct word would be displayed
